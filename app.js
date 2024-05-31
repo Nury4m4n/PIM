157c135de4a673875ec0c96c24a9f53a9c8b3de1
Nuryaman <nury4m4n13@gmail.com>
2024-05-31 20:44:18 +0700

    Perbaikan

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-// Togel class active
+const { jsPDF } = window.jspdf;
+
+const printButton = document.querySelector('#print');
+
+printButton.addEventListener('click', () => {
+  const doc = new jsPDF();
+
+  doc.text('Dilantiknya Ketua BEM 2024/2025', 10, 20);
+  doc.text(`Telah dipilih sebagai Ketua Badan Eksekutif Mahasiswa (BEM) saudara Faisal untuk masa bakti mendatang, yang
+    akan bersama-sama ditemani oleh sosok yang sama-sama luar biasa, Flora, sebagai Wakil Ketua. Tidak hanya
+    sekedar pemimpin, mereka adalah representasi dari harapan dan aspirasi mahasiswa seluruhnya, yang
+    mempercayakan mereka untuk memimpin dengan integritas, dedikasi, dan keberanian.`, 10, 30);
+
+  doc.save("informasi.pdf");
+});
 
 const navbar = document.querySelector('.navbar-nav');
 const burger = document.querySelector('#hamburger-menu')
@@ -10,8 +24,3 @@ document.addEventListener('click', (e) => {
     navbar.classList.remove('active')
   }
 });
-
-
-
-
-
